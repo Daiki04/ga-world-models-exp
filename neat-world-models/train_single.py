0e2e75b5d01475f61ef76a69caacd3b37ccb3ec5
Daiki04 <daikiwanwan04@gmail.com>
2023-11-02 10:03:51 +0900

    add reset

--- neat-world-models/train_single.py
+++ neat-world-models/train_single.py
@@ -42,58 +42,6 @@ def eval_genomes(genomes, configs):
     for _, gC in genomeC:
         gC.fitness = 0.0
 
-    # for (idV, gV), (idM, gM), (idC, gC) in zip(genomeV, genomeM, genomeC):
-    #     time_count = 0
-    #     netV = neat.nn.FeedForwardNetwork.create(gV, configV) # VAE
-    #     netM = neat.nn.RecurrentNetwork.create(gM, configM) # MDRNN
-    #     netC = neat.nn.FeedForwardNetwork.create(gC, configC) # Controller
-
-    #     cumulative = 0
-
-    #     hidden = np.zeros(RSIZE) # 隠れ状態
-
-    #     env = gym.make('CarRacing-v2', render_mode='rgb_array', domain_randomize=False) # 環境：CarRacing-v2
-    #     obs, _ = env.reset() # 環境をリセット
-    #     neg_reward = 0
-
-    #     while True:
-    #         obs = transform(obs).unsqueeze(0) # 画像を前処理
-    #         obs = obs.flatten().detach().numpy() # 画像をベクトルに変換
-
-    #         latent_mu_sigma= netV.activate(obs)
-    #         latent_mu_sigma = np.array(latent_mu_sigma)
-    #         latent_mu = latent_mu_sigma[:LSIZE]
-    #         latent_sigma = latent_mu_sigma[LSIZE:]
-    #         latent = latent_mu + latent_sigma * np.random.normal(size=latent_mu.shape)
-    #         C_input = np.concatenate([latent, hidden])
-    #         action = netC.activate(C_input)
-    #         M_input = np.concatenate([action, latent])
-    #         hidden = netM.activate(M_input)
-
-    #         obs, reward, done, _, _ = env.step(action) # 行動を起こす
-    #         cumulative += reward
-
-    #         if reward < 0:
-    #             neg_reward += 1
-    #         else:
-    #             neg_reward = 0
-
-    #         if time_count > time_limit or neg_reward > 20:
-    #             done = True
-            
-    #         if done:
-    #             env.close()
-    #             if gM.fitness < cumulative:
-    #                 gM.fitness = cumulative
-    #             if gC.fitness < cumulative:
-    #                 gC.fitness = cumulative
-    #             if gV.fitness < cumulative:
-    #                 gV.fitness = cumulative
-    #             break
-
-    #         time_count += 1
-    #     print(f"id:[{idV}, {idM}, {idC}], fitness: {cumulative}")
-
     for idV, gV in genomeV:
         for idM, gM in genomeM:
             for idC, gC in genomeC:
